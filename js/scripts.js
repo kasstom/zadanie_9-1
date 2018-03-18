bfd011b08d20c8c859a7d9f49ddf99a54af093e3
Aneta Koppernock <akozuchowska@hotmail.com>
2018-03-18 23:35:43 +0100

    add file

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+
+function getTriangleArea(a, h) {
+    if ((a > 0) && (h > 0)) {
+        return a * h / 2;
+    } else {
+        console.log('nieprawidlowe dane');
+    }
+}
+
+var triangle1Area = getTriangleArea(-10, 15);
+var triangle2Area = getTriangleArea(10, 4);
+var triangle3Area = getTriangleArea(3, 6);
+
+console.log(triangle1Area);
+console.log(getTriangleArea(10, 6));
+console.log(triangle2Area);
+console.log(triangle3Area);
